9ac83668ee892f4edcde16aa457ce329c022505c
TheRogueNico <theroguenico@gmail.com>
2026-07-29 15:39:21 +0330

    Create the basic functionality

--- brush.go
+++ brush.go
@@ -1,2 +1,20 @@
 // Package brush styles terminal text with ANSI colors.
 package brush
+
+import "fmt"
+
+type Color int
+
+const Red Color = 31
+
+func (c Color) Style(text string) string {
+	return "\033[31m" + text + "\033[0m"
+}
+
+type Styler interface {
+	Style(text string) string
+}
+
+func Paint(s Styler, a ...any) string {
+	return s.Style(fmt.Sprint(a...))
+}
