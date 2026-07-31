698425449c0aa95d130e3a754d40810c59c49cbf
TheRogueNico <theroguenico@gmail.com>
2026-07-31 13:26:05 +0330

    Add String method for Color

--- brush.go
+++ brush.go
@@ -51,3 +51,17 @@ func (c Color) Paint(s string) string {
 	}
 	return esc + strconv.Itoa(c.fgCode()) + end + s + reset
 }
+
+var colorNames = [...]string{
+	"NoColor",
+	"Black", "Red", "Green", "Yellow", "Blue", "Magenta", "Cyan", "White",
+	"BrightBlack", "BrightRed", "BrightGreen", "BrightYellow",
+	"BrightBlue", "BrightMagenta", "BrightCyan", "BrightWhite",
+}
+
+func (c Color) String() string {
+	if c < 0 || int(c) >= len(colorNames) {
+		return "Color(" + strconv.Itoa(int(c)) + ")"
+	}
+	return colorNames[c]
+}
